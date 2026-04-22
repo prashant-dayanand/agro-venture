6246fdced2b70e3c2bb263846769c34cc2f063fb
parshant <jangadaprashant@gmail.com>
2026-04-22 13:34:15 +0530

    code update

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,35 +1,43 @@
-// lib/mongodb.js
+// lib/mongodb.ts
 
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-console.log(MONGODB_URI)
-
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-let cached  = global.mongoose;
+type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongooseCache: MongooseCache | undefined;
+}
+
+let cached = global.mongooseCache;
 
 if (!cached) {
-  cached = global.mongoose = {
+  cached = global.mongooseCache = {
     conn: null,
     promise: null,
   };
 }
 
 export async function connectToDatabase() {
-  if (cached.conn) return cached.conn;
+  if (cached!.conn) {
+    return cached!.conn;
+  }
 
-  console.log(MONGODB_URI)
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
+  if (!cached!.promise) {
+    cached!.promise = mongoose.connect(MONGODB_URI, {
       bufferCommands: false,
     });
   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+  cached!.conn = await cached!.promise;
+  return cached!.conn;
 }
