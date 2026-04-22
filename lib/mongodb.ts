208dca2865e72e104bf9164641590e46576b6687
parshant <jangadaprashant@gmail.com>
2026-04-22 13:43:39 +0530

    code update

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,12 +2,6 @@
 
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
-}
-
 type MongooseCache = {
   conn: typeof mongoose | null;
   promise: Promise<typeof mongoose> | null;
@@ -28,6 +22,12 @@ if (!cached) {
 }
 
 export async function connectToDatabase() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable");
+  }
+
   if (cached!.conn) {
     return cached!.conn;
   }
